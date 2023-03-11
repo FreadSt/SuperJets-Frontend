e705e07a404f405c4009b54f1ff102dce73c1b4e
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-11 23:48:52 +0200

    added save prices

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,6 +5,14 @@ export const Navbar = () => {
     const [price_xnl, setPriceXNL] = useState("0.0");
     const [price_eth, setPriceETH] = useState("0.0");
 
+    const savePrices = () => {
+        fetch(`/contract/set_prices?price_eth=${price_eth}&price_xnl=${price_xnl}`, {
+            method: "POST",
+        }).catch((err)=>{
+            console.log("Save price error");
+        });
+    }
+
     fetch("/contract/prices").then((res)=>{
         res.json().then((res)=>{
             setPriceETH(res.price_eth);
@@ -23,7 +31,9 @@ export const Navbar = () => {
                     <button>
                         <span>ETH {price_xnl}</span>
                     </button>
-                    <button className={'save-btn'}>
+                    <button className={'save-btn'}
+                    onClick={ savePrices }
+                    >
                         <span>Save</span>
                     </button>
                 </div>
