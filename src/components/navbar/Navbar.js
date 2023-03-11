73106114e72e3d7a13bf781cf9973585066a10d4
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-11 15:52:09 +0200

    added states of prices to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,27 @@
-import './style.scss'
+import './style.scss';
+import {useState} from "react";
 
 export const Navbar = () => {
+    const [price_xnl, setPriceXNL] = useState();
+    const [price_eth, setPriceETH] = useState();
+
+    fetch("/contract/prices").then((res)=>{
+        res.json().then((res)=>{
+            setPriceETH(res.price_eth);
+            setPriceXNL(res.price_xnl);
+        })
+    })
+
     return(
         <div className={'navbar'}>
             <div className={'navbar-content'}>
                 <span>Pack price:</span>
                 <div className={'buttons-group'}>
                     <button>
-                        <span>XNL 0.00</span>
+                        <span>XNL {price_eth}</span>
                     </button>
                     <button>
-                        <span>ETH 0.00</span>
+                        <span>ETH {price_xnl}</span>
                     </button>
                     <button className={'save-btn'}>
                         <span>Save</span>
